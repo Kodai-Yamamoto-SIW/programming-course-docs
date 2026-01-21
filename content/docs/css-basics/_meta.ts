9529553fd827fda9f2a5c29f166ddbea52b82155
metyatech <metyatech@gmail.com>
2026-01-21 15:35:24 +0900

    content: move docs and assets to content/public

---
+++ content/docs/css-basics/_meta.ts
@@ -0,0 +1,23 @@
+const meta = {
+  "css-introduction": {},
+  "css-units": {},
+  "css-styling-basics": {},
+  "box-model": {},
+  "text-typography": {},
+  "advanced-styling": {},
+  "pseudo-classes": {},
+  "css-animations": {},
+  "css-selectors": {},
+  "backgrounds": {},
+  "shorthand-properties": {},
+  "decorative-effects": {},
+  "positioning": {},
+  "negative-margin": {},
+  "display": {},
+  "flexbox": {},
+  "flexbox-advanced": {},
+  "layout-summary-exercises": {}
+};
+
+export default meta;
+
