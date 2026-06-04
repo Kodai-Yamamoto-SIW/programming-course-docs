1f7f9a67d151f33a9151dd2008b5213a8d256b8d
metyatech <metyatech@gmail.com>
2026-06-04 19:44:14 +0900

    実サイト風カンプからのマークアップ　を追加

--- content/docs/html-basics/practice-exercises/_meta.ts
+++ content/docs/html-basics/practice-exercises/_meta.ts
@@ -3,6 +3,7 @@ const meta = {
   'markup-exercises-advanced': {},
   'design-comp-introduction': {},
   'styled-design-comp-markup': {},
+  'styled-design-comp-markup-advanced': {},
 };
 
 export default meta;
